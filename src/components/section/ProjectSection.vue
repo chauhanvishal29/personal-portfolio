b5bfc83015dea69dca77706c37a8a4d82378dd5f
vishal <chauhanvishalb1@gmail.com>
2024-11-10 15:27:47 +0530

    update project section

--- src/components/section/ProjectSection.vue
+++ src/components/section/ProjectSection.vue
@@ -6,14 +6,14 @@ const projects = [
     {
         image: Project1,
         project_name: 'Employee Payroll',
-        technology: ['python', 'Html','css'],
-        link:'https://github.com/chauhanvishal29/college-management'
+        technology: ['python', 'Html', 'css'],
+        link: 'https://github.com/chauhanvishal29/college-management'
     },
     {
         image: Project2,
         project_name: 'Electrical Service',
-        technology: ['Laravel','html','css'],
-        link:'https://github.com/chauhanvishal29/electrical-service'
+        technology: ['Laravel', 'html', 'css'],
+        link: 'https://github.com/chauhanvishal29/electrical-service'
     }
 ];
 </script>
@@ -35,14 +35,12 @@ const projects = [
                 <div v-for="(item, index) in projects" :key="index" class="relative group">
                     <div
                         class="mb-5 md:block flex group-hover:opacity-25 transition-opacity duration-300 ease-out justify-center">
-                        <img :src="item.image" class="md:w-[540px] group-hover:opacity-50 transition-opacity duration-300 ease-out w-[346px] h-[400px] object-cover " alt="">
+                        <img :src="item.image"
+                            class="md:w-[540px] w-[345px] group-hover:opacity-50 transition-opacity duration-300 ease-out h-[400px] object-cover "
+                            alt="">
                     </div>
-                    <div class="absolute hidden group-hover:flex  -translate-x-2/4 -translate-y-2/4 left-2/4 top-[37%]">
-                        <a :href="item.link"
-                            class="no-underline uppercase text-white tracking-widest border-b-2 border-eucalyptus hover:text-eucalyptus transition-all duration-[0.2s] ease-linear text-base font-bold leading-6 tracking-normal pb-4">View
-                            Code</a>
-                    </div>
-                    <div class="text-white max-w-[345px] my-0 mx-auto uppercase font-bold leading-[3.2rem]">
+                    <div
+                        class="text-white md:max-w-[540px] max-w-[345px] my-0 mx-auto uppercase font-bold leading-[3.2rem]">
                         <h3 class="text-2xl tracking-normal">{{ item.project_name }}</h3>
                         <div class="flex space-x-5 pb-5 pt-2">
                             <span v-for="(tech, techIndex) in item.technology" :key="techIndex"
@@ -50,6 +48,12 @@ const projects = [
                                 {{ tech }}
                             </span>
                         </div>
+                    </div>
+                    <div
+                        class="lg:absolute relative lg:hidden block top-0 group-hover:flex md:max-w-[540px] max-w-[345px] -translate-x-2/4 -translate-y-2/4 left-2/4 lg:top-[37%]">
+                        <a :href="item.link"
+                            class="no-underline uppercase text-white tracking-widest border-b-2 border-eucalyptus hover:text-eucalyptus transition-all duration-[0.2s] ease-linear text-base font-bold leading-[2px] lg:pb-3 pb-1">View
+                            Code</a>
                     </div>
                 </div>
             </div>
